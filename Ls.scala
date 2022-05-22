336d3ab5c60cbc4cc28d83e39493227b26d7036b
Łukasz Wroński <lukaszwronski98@gmail.com>
2022-05-22 17:50:38 +0200

    Build ls app using scala-cli

---
+++ Ls.scala
@@ -0,0 +1,10 @@
+//> using scala "3.1.2"
+//> using lib "com.lihaoyi::os-lib:0.8.0"
+
+@main def hello(args: String*) =
+  val path = args.headOption match
+    case Some(p) => os.Path(p, os.pwd)
+    case _       => os.pwd
+
+  if (os.isDir(path)) println(os.list(path).mkString(","))
+  else System.err.println("Expected directory path as an input")
